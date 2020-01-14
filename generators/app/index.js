460fa2522d4063bd68010068d81116d5be26b3cf
twebber90 <twebber@tractorzoom.com>
2020-01-14 08:38:53 -0600

    feat: add commitlint, prettier, renovate and terraform generators

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,8 +1,169 @@
 var Generator = require("yeoman-generator");
+const fs = require("fs-extra");
+
+const addCommitlintConfiguration = context => {
+  context.log("Adding Commitlint configuration");
+
+  const pkgJson = {
+    config: {
+      commitizen: {
+        path: "cz-conventional-changelog"
+      }
+    },
+    devDependencies: {
+      "@commitlint/cli": "8.2.0",
+      "@commitlint/config-conventional": "8.2.0",
+      commitizen: "4.0.3",
+      husky: "3.1.0"
+    },
+    scripts: {
+      commit: "git-cz"
+    }
+  };
+
+  context.fs.extendJSON(context.destinationPath("package.json"), pkgJson);
+
+  fs.copyFileSync(
+    context.templatePath("commitlint.config.js"),
+    context.destinationPath("commitlint.config.js")
+  );
+};
+
+const addPrettierConfiguration = context => {
+  context.log("Adding Prettier configuration");
+
+  const pkgJson = {
+    devDependencies: {
+      husky: "3.1.0",
+      prettier: "1.19.1",
+      "pretty-quick": "2.0.1"
+    },
+    scripts: {
+      "pretty-quick": "pretty-quick"
+    }
+  };
+
+  context.fs.extendJSON(context.destinationPath("package.json"), pkgJson);
+
+  fs.copyFileSync(
+    context.templatePath("prettier.config.js"),
+    context.destinationPath("prettier.config.js")
+  );
+};
+
+const addRenovateConfiguration = context => {
+  context.log("Adding Renovate configuration");
+
+  const renovateJSON = {
+    extends: ["config:base"],
+    prConcurrentLimit: 5,
+    prHourlyLimit: 10
+  };
+
+  context.fs.extendJSON(context.destinationPath("renovate.json"), renovateJSON);
+};
+
+const addTerraformConfiguration = context => {
+  context.log("Adding Terraform configuration");
+
+  const pkgJson = {
+    scripts: {
+      "terraform:fmt": "sh terraform-format.sh"
+    }
+  };
+
+  context.fs.extendJSON(context.destinationPath("package.json"), pkgJson);
+
+  fs.copyFileSync(
+    context.templatePath("terraform-format.sh"),
+    context.destinationPath("terraform-format.sh")
+  );
+};
+
+const addHuskyConfiguration = context => {
+  const preCommitTasks = [];
+
+  if (context.answers.prettier) {
+    preCommitTasks.push("'pretty-quick --staged'");
+  }
+
+  if (context.answers.terraform) {
+    preCommitTasks.push("'npm run terraform:fmt'");
+  }
+
+  const commitMessageHook = context.answers.commitlint
+    ? "        'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',\n"
+    : "";
+
+  const huskyConfig =
+    "const tasks = arr => arr.join(' && ')\n\n" +
+    "module.exports = {\n" +
+    "    hooks: {\n" +
+    commitMessageHook +
+    `        'pre-commit': tasks([${preCommitTasks.join(", ")}]),\n` +
+    "    },\n}\n";
+
+  fs.writeFileSync(context.destinationPath(".huskyrc.js"), huskyConfig);
+};
 
 module.exports = class extends Generator {
-  initializing() {
-    this.composeWith(require.resolve("../name"));
-    this.composeWith(require.resolve("../cool"));
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        type: "confirm",
+        name: "commitlint",
+        message: "Would you like to enable pre-commit hook for Commitlint?",
+        store: true
+      },
+      {
+        type: "confirm",
+        name: "prettier",
+        message: "Would you like to enable pre-commit hook for Prettier?",
+        store: true
+      },
+      {
+        type: "confirm",
+        name: "renovate",
+        message: "Would you like to enable Renovate?",
+        store: true
+      },
+      {
+        type: "confirm",
+        name: "terraform",
+        message: "Would you like to enable pre-commit hook for Terraform ?",
+        store: true
+      }
+    ]);
+  }
+
+  writing() {
+    const shouldAddHuskyConfig =
+      this.answers.commitlint ||
+      this.answers.prettier ||
+      this.answers.terraform;
+
+    if (this.answers.commitlint) {
+      addCommitlintConfiguration(this);
+    }
+
+    if (this.answers.prettier) {
+      addPrettierConfiguration(this);
+    }
+
+    if (this.answers.renovate) {
+      addRenovateConfiguration(this);
+    }
+
+    if (this.answers.terraform) {
+      addTerraformConfiguration(this);
+    }
+
+    if (shouldAddHuskyConfig) {
+      addHuskyConfiguration(this);
+    }
+  }
+
+  installing() {
+    this.npmInstall();
   }
 };
